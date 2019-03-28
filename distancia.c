d89e8d8cd4a04fa698f7d056089245c96ecb3910
imorem00 <47975473+imorem00@users.noreply.github.com>
2019-03-28 23:02:23 +0100

    Add files via upload

---
+++ distancia.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+	
+	void cargar(int v[2]);
+	float distancia(int v1[2],int v2[2]);
+	int main()
+{
+	int v1[2];int v2[2];
+	cargar (v1);
+	cargar (v2);
+	float de=distancia(v1,v2);
+	printf("La distancia es %f\n",de);
+	return 0;
+}
+	void cargar (int v[2])
+{
+	int i;
+	for(i=0;i<2;i++)
+{
+	printf("Escribe las coordenadas %d\n",i++);
+	scanf("%d",&v[i]);
+}
+}
+	float distancia(int v1[2],int v2[2]){
+	int resta1=v2[0]-v1[0];
+	int resta2=v2[1]-v1[1];
+	float raiz=srqt(pow(resta1,2) + pow(resta2,2));
+	return raiz;
+}
